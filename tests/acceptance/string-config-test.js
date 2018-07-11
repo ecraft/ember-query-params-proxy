f6d69fe898a518d29e505c8e7261b8d23e8cdd59
Olle Jonsson <olle.jonsson@gmail.com>
2018-07-11 17:01:03 +0200

    Tests: fix more of the acceptance test

--- tests/acceptance/string-config-test.js
+++ tests/acceptance/string-config-test.js
@@ -1,18 +1,19 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { fillIn, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 
-moduleForAcceptance('Acceptance | string config');
+module('Acceptance | string config', function(hooks) {
+    setupApplicationTest(hooks);
 
-test('testing string-config', function(assert) {
-  visit('/');
-  andThen(function() {
-    assert.equal(find('[data-test="foo-output"]').text(), '42', 'foo is set according to controller default');
-    assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar is set according to controller default');
-  });
-  fillIn('[data-test="foo-input"]', '43');
-  andThen(function() {
-    assert.equal(find('[data-test="foo-output"]').text(), '43', 'foo is updated by action');
-    assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar remains default');
-    assert.equal(currentURL(), '/?foo=43', 'URL contains the foo query parameter');
-  });
+    test('testing string-config', async function (assert) {
+        await visit('/');
+
+        assert.equal(find('[data-test="foo-output"]').text(), '42', 'foo is set according to controller default');
+        assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar is set according to controller default');
+
+        await fillIn('[data-test="foo-input"]', '43');
+        assert.equal(find('[data-test="foo-output"]').text(), '43', 'foo is updated by action');
+        assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar remains default');
+        assert.equal(currentURL(), '/?foo=43', 'URL contains the foo query parameter');
+    });
 });
