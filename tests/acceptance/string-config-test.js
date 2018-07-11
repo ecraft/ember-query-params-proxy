e7afd2e4f2aaffc29f937fd0900483f7e60a6eac
Olle Jonsson <olle.jonsson@gmail.com>
2018-07-11 20:41:24 +0200

    Test fix: Use assert.dom

--- tests/acceptance/string-config-test.js
+++ tests/acceptance/string-config-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { fillIn, visit } from '@ember/test-helpers';
+import { currentURL, fillIn, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | string config', function(hooks) {
@@ -8,12 +8,12 @@ module('Acceptance | string config', function(hooks) {
     test('testing string-config', async function (assert) {
         await visit('/');
 
-        assert.equal(find('[data-test="foo-output"]').text(), '42', 'foo is set according to controller default');
-        assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar is set according to controller default');
+        assert.dom('[data-test="foo-output"]').hasText('42', 'foo is set according to controller default');
+        assert.dom('[data-test="bar-output"]').hasText('someString', 'bar is set according to controller default');
 
         await fillIn('[data-test="foo-input"]', '43');
-        assert.equal(find('[data-test="foo-output"]').text(), '43', 'foo is updated by action');
-        assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar remains default');
+        assert.dom('[data-test="foo-output"]').hasText('43', 'foo is updated by action');
+        assert.dom('[data-test="bar-output"]').hasText('someString', 'bar remains default');
         assert.equal(currentURL(), '/?foo=43', 'URL contains the foo query parameter');
     });
 });
