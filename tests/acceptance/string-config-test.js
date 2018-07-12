b9d391a7465ca9d69e6b430d6b1289ff818c57d5
Olle Jonsson <olle.jonsson@gmail.com>
2018-07-12 09:12:44 +0200

    Update Ember CLI (#3)
    * ember-cli-update --from 2.18.0
    
    * ESLint globals added
    
    * Tests: fix more of the acceptance test
    
    * Test fix: Use assert.dom
    
    * Ember Try: useYarn: true
    
    * yarn.lock update
    
    * eslint: Remove spurious configuration
    
    * Add me as maintainer

--- tests/acceptance/string-config-test.js
+++ tests/acceptance/string-config-test.js
@@ -1,18 +1,19 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { currentURL, fillIn, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 
-moduleForAcceptance('Acceptance | string config');
+module('Acceptance | string config', function(hooks) {
+    setupApplicationTest(hooks);
 
-test('testing string-config', function(assert) {
-  visit('/');
-  andThen(function() {
-    assert.equal(find('[data-test="foo-output"]').text(), '42', 'foo is set according to controller default');
-    assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar is set according to controller default');
-  });
-  fillIn('[data-test="foo-input"]', '43');
-  andThen(function() {
-    assert.equal(find('[data-test="foo-output"]').text(), '43', 'foo is updated by action');
-    assert.equal(find('[data-test="bar-output"]').text(), 'someString', 'bar remains default');
-    assert.equal(currentURL(), '/?foo=43', 'URL contains the foo query parameter');
-  });
+    test('testing string-config', async function (assert) {
+        await visit('/');
+
+        assert.dom('[data-test="foo-output"]').hasText('42', 'foo is set according to controller default');
+        assert.dom('[data-test="bar-output"]').hasText('someString', 'bar is set according to controller default');
+
+        await fillIn('[data-test="foo-input"]', '43');
+        assert.dom('[data-test="foo-output"]').hasText('43', 'foo is updated by action');
+        assert.dom('[data-test="bar-output"]').hasText('someString', 'bar remains default');
+        assert.equal(currentURL(), '/?foo=43', 'URL contains the foo query parameter');
+    });
 });
